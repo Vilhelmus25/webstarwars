4ff3612b53a107080f379af389b94499310639ab
Vilhelmus25 <vorvie@gmail.com>
2021-12-09 16:33:55 +0100

    simulate fonts

--- frontend/src/app/pages/simulate-fight/simulate-fight.component.ts
+++ frontend/src/app/pages/simulate-fight/simulate-fight.component.ts
@@ -1,17 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Characters } from 'src/app/models/characters';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { CharactersService } from 'src/app/services/characters.service';
+import { ConfigService } from 'src/app/services/config.service';
 import { DataService } from 'src/app/services/data.service';
+import { UserService } from 'src/app/services/user.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-simulate-fight',
   templateUrl: './simulate-fight.component.html',
   styleUrls: ['./simulate-fight.component.scss']
 })
-export class SimulateFightComponent implements OnInit {
+export class SimulateFightComponent extends LoginComponent implements OnInit {
 
-
+  currentUser$: User = this.auth.getUser();
+  characterList$: Observable<Characters[]> = this.characterService.getAll();
+  characterList: Characters[] = [];
   message!: string[];
+  leftPercent: number = 100;
+  rightPercent: number = 100;
 
-  constructor(private data: DataService) { }
+  constructor(public userService: UserService,
+    public config: ConfigService,
+    public characterService: CharactersService,
+    public router: Router,
+    public auth: AuthService,
+    private data: DataService,
+    //private renderer: Renderer2,
+  ) {
+    super(config, auth, router);
+    this.characterList$.subscribe(itemList => {
+      this.characterList = itemList;
+      //console.log(this.characterList);
+    });
+  }
 
   ngOnInit(): void {
     this.data.currentMessage.subscribe(message => this.message = message)
